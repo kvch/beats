22d35ee4f6e67cdeedabaa06581925d2d080a058
Noémi Ványi <sitbackandwait@gmail.com>
2018-07-02 17:57:17 +0200

    add delete command

--- libbeat/cmd/pipelines.go
+++ libbeat/cmd/pipelines.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	listPipelines = "/_ingest/pipeline/%s"
+	pipelinesURL = "/_ingest/pipeline/%s"
 )
 
 // GenModulesCmd initializes a command to manage a modules.d folder, it offers
@@ -26,6 +26,7 @@ func genPipelinesCmd(name, version string) *cobra.Command {
 	}
 
 	pipelinesCmd.AddCommand(genListPipelinesCmd(name, version))
+	pipelinesCmd.AddCommand(genDeletePipelinesCmd(name, version))
 
 	return &pipelinesCmd
 }
@@ -35,38 +36,22 @@ func genListPipelinesCmd(name, version string) *cobra.Command {
 		Use:   "list [id]",
 		Short: "List pipelines",
 		Run: func(cmd *cobra.Command, args []string) {
-			b, err := instance.NewBeat(name, "", version)
+			esClient, err := setupElasticsearchClient(name, version)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error initializing beat: %s\n", err)
-				os.Exit(1)
-			}
-
-			if err = b.Init(); err != nil {
-				fmt.Fprintf(os.Stderr, "Error initializing beat: %s\n", err)
-				os.Exit(1)
-			}
-
-			if b.Config.Output.Name() != "elasticsearch" {
-				fmt.Fprintf(os.Stderr, "Error conencting to Elasticsearch: missing configuration from file\n")
+				fmt.Fprintf(os.Stderr, "%v", err)
 				os.Exit(1)
 			}
 
 			reqURL := ""
 			if len(args) == 0 {
-				reqURL = fmt.Sprintf(listPipelines, "")
+				reqURL = fmt.Sprintf(pipelinesURL, "")
 			} else if len(args) == 1 {
-				reqURL = fmt.Sprintf(listPipelines, args[0])
+				reqURL = fmt.Sprintf(pipelinesURL, args[0])
 			} else {
 				fmt.Fprintf(os.Stderr, "Too many parameters\n")
 				os.Exit(2)
 			}
 
-			esConfig := b.Config.Output.Config()
-			esClient, err := elasticsearch.NewConnectedClient(esConfig)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error conencting to Elasticsearch: %v\n", err)
-				os.Exit(1)
-			}
 			code, r, err := esClient.Request("GET", reqURL, "", nil, nil)
 			if code == 404 {
 				fmt.Println("No such pipeline")
@@ -77,14 +62,84 @@ func genListPipelinesCmd(name, version string) *cobra.Command {
 				os.Exit(1)
 			}
 
-			var resp common.MapStr
-			buf := bytes.NewBuffer(r)
-			err = json.Unmarshal(buf.Bytes(), &resp)
+			err = printResponse(r)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error during parsing response from Elasticsearch: %v\n", err)
+				fmt.Fprintf(os.Stderr, "%v", err)
 				os.Exit(3)
 			}
-			fmt.Println(resp.StringToPrint())
 		},
 	}
+}
+
+func genDeletePipelinesCmd(name, version string) *cobra.Command {
+	return &cobra.Command{
+		Use:   "delete [id]",
+		Short: "Delete pipelines",
+		Run: func(cmd *cobra.Command, args []string) {
+			esClient, err := setupElasticsearchClient(name, version)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v", err)
+				os.Exit(1)
+			}
+
+			reqURL := ""
+			if len(args) == 1 {
+				reqURL = fmt.Sprintf(pipelinesURL, args[0])
+			} else {
+				fmt.Fprintf(os.Stderr, "One parameter is required\n")
+				os.Exit(2)
+			}
+
+			code, r, err := esClient.Request("DELETE", reqURL, "", nil, nil)
+			if code == 404 {
+				fmt.Println("No such pipeline")
+				os.Exit(0)
+			}
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error during request to Elasticsearch: %v\n", err)
+				os.Exit(1)
+			}
+
+			err = printResponse(r)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v", err)
+				os.Exit(3)
+			}
+		},
+	}
+}
+
+func setupElasticsearchClient(name, version string) (*elasticsearch.Client, error) {
+	b, err := instance.NewBeat(name, "", version)
+	if err != nil {
+		return nil, fmt.Errorf("Error initializing beat: %s\n", err)
+	}
+
+	if err = b.Init(); err != nil {
+		return nil, fmt.Errorf("Error initializing beat: %s\n", err)
+	}
+
+	if b.Config.Output.Name() != "elasticsearch" {
+		return nil, fmt.Errorf("Error conencting to Elasticsearch: missing configuration from file\n")
+	}
+
+	esConfig := b.Config.Output.Config()
+	esClient, err := elasticsearch.NewConnectedClient(esConfig)
+	if err != nil {
+		return nil, fmt.Errorf("Error conencting to Elasticsearch: %v\n", err)
+	}
+	return esClient, nil
+}
+
+func printResponse(response []byte) error {
+	var resp common.MapStr
+	buf := bytes.NewBuffer(response)
+	err := json.Unmarshal(buf.Bytes(), &resp)
+	if err != nil {
+		return fmt.Errorf("Error during parsing response from Elasticsearch: %v\n", err)
+	}
+
+	fmt.Println(resp.StringToPrint())
+
+	return nil
 }
